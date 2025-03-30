a0f85a5cee6ffbc17ff14b43a5c2ca9642f3918c
DayanLegenda <slejkahd2022@gmail.com>
2025-03-31 00:40:32 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+﻿BOT_TOKEN = "7517476911:AAF_Ho5WAl_YwES_BEmJO3ofY9ggBNJ7Ujo" # токен ботика
+CHANNEL_URL = "https://t.me/+FUv0Ei9XqV9iZjEy"  # ссылка канала проверка подписки
+CHANNEL_ID = "-1002174393842"  # айди канала для проверки подписки
+VERIF_CHANNEL_ID = "-1002520359651"  # айди чата для верификации(обсуждение канала с постбеками)
+SUPP = "https://t.me/Muxa1win"
+ADMIN_ID = 5808311463 # Ваш айди без скобочек
+PROMO = "XACKGAME" # промокод
+DEFAULT_REF_LINK = "https://1wprde.com/?p=w7gh" # Добавьте эту проверку
